d0a76dbfd2a3b1ccc655c841faf854bf429daad8
RtxRulez <vafarulez@ya.ru>
2019-06-16 14:17:45 +0300

    Конфиг вебпака

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+import Signa from "./Signa/Signa";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Signa />
+      </div>
+    );
+  }
+}
+
+export default App;
