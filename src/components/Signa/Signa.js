98b0d9db8135bb3d28924074afc8b9e6e0179043
RtxRulez <vafarulez@ya.ru>
2019-06-16 14:53:17 +0300

    добавил библиотеку

--- src/components/Signa/Signa.js
+++ src/components/Signa/Signa.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import domtoimage from 'dom-to-image';
 
 class Signa extends Component {
+    componentDidMount() {
+        console.log('d', domtoimage)
+    }
     render() {
         return (
             <div>
-               Signa 
+                <div id="vartext">Text</div>
+                <h1>Signa Creater</h1>
+               <img src="./images/main.png" alt=""/>
             </div>
         )
     }
