4a635a49fab939e444025cc4d1a984fb85393fb9
Ramil <vafarulez@ya.ru>
2019-06-17 14:59:37 +0300

    интерфейс

--- src/components/Signa/Signa.js
+++ src/components/Signa/Signa.js
@@ -1,20 +1,37 @@
-import React, { Component } from 'react'
-import domtoimage from 'dom-to-image';
-import './Signa.scss'
+import React, { Component } from "react";
+import domtoimage from "dom-to-image";
+import "./Signa.scss";
 
 class Signa extends Component {
-    componentDidMount() {
-        console.log('d', domtoimage)
-    }
-    render() {
-        return (
-            <div>
-                <div id="vartext">Text</div>
-                <h1>Signa Creater</h1>
-               <img src="./images/main.png" alt=""/>
-            </div>
-        )
-    }
+  componentDidMount() {
+    setTimeout(() => {
+      var node = document.getElementById("content");
+
+      domtoimage
+        .toPng(node)
+        .then(function(dataUrl) {
+          var img = new Image();
+          img.src = dataUrl;
+          document.body.appendChild(img);
+        })
+        .catch(function(error) {
+          console.error("oops, something went wrong!", error);
+        });
+    }, 50);
+  }
+  render() {
+    return (
+      <div className="signa">
+        <div className="signa__wrapper">
+          <h1 className="signa__head">Signa Creater</h1>
+          <div className="signa__content" id="content">
+              <img src="./images/vika.png" alt="" className="signa__img"/>
+              <div className="signa__text signa__text--str1">Test TestTestTestTest</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Signa
+export default Signa;
